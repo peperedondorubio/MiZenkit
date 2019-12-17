f3947cb8a2f44364f3c43dc337fa2e77203fa162
PepeRedondoRubio <peperedondorubio@gmail.com>
2019-12-17 12:37:23 +0100

    modificado:     .idea/workspace.xml 	modificado:     segun.py         Uso del PUT y otras cosas

--- segun.py
+++ segun.py
@@ -5,7 +5,6 @@ urlbase = 'https://zenkit.com/api/v1/'
 cabecera = {'Content-Type': 'application/json',
             'Zenkit-API-Key': 'jffaf7ne-X1ELSpJZLdRlE5uAWIbWC47rJlna51h2'}
 
-
 def get_account_info():
     url = urlbase + "auth/currentuser"
     response = requests.get(url, headers=cabecera)
@@ -14,6 +13,7 @@ def get_account_info():
         return json.loads(response.content.decode('utf-8'))
     else:
         return None
+
 
 def get_ws():
     url = urlbase + "users/me/workspacesWithLists"
@@ -24,24 +24,45 @@ def get_ws():
     else:
         return None
 
+def jsonAFichero(df):
+    fichero = "/tmp/prueba.json"
+
+    with open(fichero, 'w') as file:
+        json.dump(df, file)
+        file.close()
+
+def put_cambioElemento(listId, listEntryId, elementId, elementChange):
+    url = urlbase + "/api/v1/lists/" + listId + "/entries/" + listEntryId + "/elements/" + elementId
+    response = requests.put(url, headers=cabecera, params=elementChange)
+
+    if response.status_code == 200:
+        return json.loads(response.content.decode('utf-8'))
+    else:
+        return None
+
+###################################################
+#### Inicio del programa
+###################################################
 
 
 account_info = get_account_info()
 ws = get_ws()
+jsonAFichero(ws)
 
 if account_info is not None:
     print("En linea: ")
-    #for k, v in account_info.items():
+    # for k, v in account_info.items():
     #    print('{0}:{1}'.format(k, v))
 else:
-    print('[!] Error')
+    print('[!] Error1')
     exit
 
 if ws is not None:
-    for k, v in ws[1].items():
+    for k, v in ws[0]["lists"][0]["settings"]["calendarSync"].items():
         print('{0}:{1}'.format(k, v))
-    #print (ws)
+
 else:
-    print('[!] Error')
+    print('[!] Error2')
     exit()
 
+print(ws[0]["lists"][0]["settings"]["calendarSync"]["calendarName"])
