e8e2126390bae0c5295cdff9df98cc1da4bf50aa
PepeRedondoRubio <peperedondorubio@gmail.com>
2019-12-23 11:14:11 +0100

    Versión 1.2
    	modificado:     .idea/workspace.xml
    	modificado:     CreaZk.py
    
    Añadida la fecha al crear Temita

--- CreaZk.py
+++ CreaZk.py
@@ -6,33 +6,50 @@ version = "1.0"
 urlbase = 'https://zenkit.com/api/v1/'
 cabecera = {'Content-Type': 'application/json',
             'Zenkit-API-Key': 'jffaf7ne-X1ELSpJZLdRlE5uAWIbWC47rJlna51h2'}
+listShortId = "H1bSOMA9f"  # Lista de Mis Cosas
+estadoHoy = "1190201"  # Estado Hoy
+uuidEstadoHoy = "2c4f48c2-1567-4991-8289-9552d5a2b81f_categories"  # uuid Estado Hoy
+uuidVencimiento = "98029764-6bbe-485d-b4b1-1c4d80809446_date"  # uuid vencimiento
+nuevoTemita = "7fba08c0-5c02-4c73-a373-8609ffb2aea5_text" # uuid Texto de la tarea
 
-def crearTareaMiLista(listShortId,textoTarea):
+
+def crearTareaMiLista(listShortId, argumentos):
     url = urlbase + "lists/" + listShortId + "/entries"
 
-    payload = '{   \
-    "sortOrder": "highest",   \
-    "7fba08c0-5c02-4c73-a373-8609ffb2aea5_text": '
-    payload += '"' + textoTarea + '",  \
-    "2c4f48c2-1567-4991-8289-9552d5a2b81f_categories": "1190201"  \
-    }'
+    vencimiento=""
+
+    textoTarea=argumentos[1]
+
+    payload = '{  "sortOrder": "lowest",  "'
+    payload += nuevoTemita + '": '
+    payload += '"' + textoTarea + '",  "'
+    payload += uuidEstadoHoy + '": "' + estadoHoy + '"'
+    payloadFin = ' }'
+
+    if len(argumentos) == 1:
+        return None   # Sin argumentos
+    if len(argumentos) == 2:
+        payload += payloadFin
+    if len(argumentos) == 3:
+        vencimiento=argumentos[2]
+        payloadVencimiento = ', "' + uuidVencimiento + '": "' + vencimiento + '"'
+        payload += payloadVencimiento + payloadFin
+    if len(argumentos) > 3:
+        return None
 
     response = requests.post(url, data=payload, headers=cabecera)
     if response.status_code == 200:
         return json.loads(response.content.decode('utf-8'))
     else:
+        print(payload)
         return None
 
 ###############################################################################
 
-listShortId = "H1bSOMA9f"  # Lista de Mis Cosas
+jsonTarea = crearTareaMiLista(listShortId, sys.argv)
+if jsonTarea is None:
+    print('Error en la creación: ', jsonTarea)
+    exit(1)
 
-if len(sys.argv) > 1:
-    jsonTarea = crearTareaMiLista(listShortId, sys.argv[1])
-    if jsonTarea is None:
-        print('Error en la creación: ', jsonTarea)
-        exit(1)
-else:
-    exit(2)
+exit(0)       #  Está bien
 
-exit (0)
