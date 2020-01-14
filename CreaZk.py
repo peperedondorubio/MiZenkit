7e35177624c5a66aa94dc322cc4aa9f408643c77
PepeRedondoRubio <peperedondorubio@gmail.com>
2020-01-14 23:19:09 +0100

    modificado:     .idea/workspace.xml 	modificado:     CreaZk.py
    Reorganización de código y preparando para hacer paquete con una libreria de funciones para Zenkit y separar dos scripts para ser ejecutados uno de Mis cosas y otro de trabajo

--- CreaZk.py
+++ CreaZk.py
@@ -2,10 +2,13 @@ import requests
 import json
 import sys
 
+# Estáticas
 version = "1.0"
 urlbase = 'https://zenkit.com/api/v1/'
 cabecera = {'Content-Type': 'application/json',
             'Zenkit-API-Key': 'jffaf7ne-X1ELSpJZLdRlE5uAWIbWC47rJlna51h2'}
+
+# Estáticas de "Mis Cosas"
 listShortId = "H1bSOMA9f"  # Lista de Mis Cosas
 estadoHoy = "1190201"  # Estado Hoy
 uuidEstadoHoy = "2c4f48c2-1567-4991-8289-9552d5a2b81f_categories"  # uuid Estado Hoy
@@ -13,13 +16,9 @@ uuidVencimiento = "98029764-6bbe-485d-b4b1-1c4d80809446_date"  # uuid vencimient
 nuevoTemita = "7fba08c0-5c02-4c73-a373-8609ffb2aea5_text" # uuid Texto de la tarea
 
 
-def crearTareaMiLista(listShortId, argumentos):
+def crearTareaMiLista(listShortId, argumentos, nuevoTemita, uuidEstadoHoy, estadoHoy, uuidVencimiento):
+
     url = urlbase + "lists/" + listShortId + "/entries"
-
-    if len(argumentos) == 1:
-        return None   # Sin argumentos
-
-    vencimiento=""
     textoTarea=argumentos[1]
 
     payload = '{  "sortOrder": "lowest",  "'
@@ -46,10 +45,13 @@ def crearTareaMiLista(listShortId, argumentos):
 
 ###############################################################################
 
-jsonTarea = crearTareaMiLista(listShortId, sys.argv)
-if jsonTarea is None:
+# No tenemos parámetros
+if len(sys.argv) == 1:
     print('Error en la creación: ', jsonTarea)
     exit(1)
 
-exit(0)       #  Está bien
+# Crea nueva tarea
+jsonTarea = crearTareaMiLista(listShortId, sys.argv, nuevoTemita, uuidEstadoHoy, estadoHoy, uuidVencimiento)
+
+exit(0)       # Está todo bien
 
